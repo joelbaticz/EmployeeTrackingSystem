ef8e8cdc7f7c67d01ccc3ca9131ee919c399f371
Joel Baticz <joel.baticz@gmail.com>
2016-09-26 19:19:45 +1000

    Add files via upload

---
+++ ETS.domain/EmpHour.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETS.domain
+{
+    public class EmpHour
+    {
+        public int EmpHourID { get; set; }
+        public int EmpID { get; set; }
+        public DateTime WorkDate { get; set; }
+        public int Hours { get; set; }
+
+
+    }
+}
---
+++ ETS.domain/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETS.domain
+{
+    public class Employee
+    {
+        public int EmpID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime DOB { get; set; }
+        public string Phone { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstName;
+            }
+        }
+
+        public override string ToString()
+        {
+            return EmpID + ", " + FirstName + ", " + LastName + ", " + Email + ", " + DOB + ", " + Phone;
+        }
+
+
+    }
+}
